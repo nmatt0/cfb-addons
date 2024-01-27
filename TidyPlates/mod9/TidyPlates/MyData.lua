e12f3050f050c40144abbdb4f250519b0f057d80
Matt Brown <matt@nmatt.com>
2024-01-27 01:40:32 -0500

    test

--- TidyPlates/mod9/TidyPlates/MyData.lua
+++ TidyPlates/mod9/TidyPlates/MyData.lua
@@ -2,13 +2,15 @@
 
 
 icon1 = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_1"
+icon2 = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_2"
 
 
 boxdata = {}
 
 team1 = {["name"]="Shabbat",["icon"]=icon1,["members"]={["Shabbat"]=true,["Shabbatalpha"]=true,["Shabbatbeta"]=true,["Shabbatgamma"]=true,["Shabbatomega"]=true}}
-teamtest = {["name"]="Shabbat",["icon"]=icon1,["members"]={["Orgrimmar Grunt"]=true}}
+teamtest = {["name"]="Shabbat",["icon"]=icon2,["members"]={["Orgrimmar Grunt"]=true}}
 
+-- add 
 table.insert(boxdata,team1)
 table.insert(boxdata,teamtest)
 
